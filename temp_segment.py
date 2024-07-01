658d1920ed32692259a4a5bd621fbaffbd558e27
root <root@ip-172-31-68-160.ec2.internal>
2024-06-30 23:29:29 -0800

    SAM automatization

---
+++ temp_segment.py
@@ -0,0 +1,6 @@
+from utils.pollux_utils import frame_video,segment_frames_from_video
+
+if __name__ == "__main__":
+    import sys
+    video_name = sys.argv[1]
+    segment_frames_from_video(video_name)
